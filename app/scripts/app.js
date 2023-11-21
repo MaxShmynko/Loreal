9b2b8a3e2273de0e9e514d4655cbac0b5dfce951
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-11-21 19:56:41 +0300

    added a form check and error text and the name of the uploaded photo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,24 +39,65 @@ $(() => {
         }
     );
 
-    $("#submit-button").on("click", function () {
-        const formData = {
-            photo: $(".form__loading-input-file input").val(),
-            expert: $(".form__radio-container-btn input:checked").val(),
-            description: $("#counter-text").val()
-        };
+    $(".form__loading-input-file input").on("change", function () {
+        const fileName = $(this).val().split("\\").pop();
 
-        $.ajax({
-            type: "POST",
-            url: "https://podruzhka.woman.ru/save_data/",
-            data: formData,
-            success: function (response) {
-                console.log("Данные успешно отправлены", response);
-            },
-            error: function (xhr, status, error) {
-                console.error("Ошибка при отправке данных", xhr, status, error);
-            }
-        });
+        $(".form__button-file-name").text(fileName);
     });
 
+    $("#submit-button").on("click", function () {
+        var isValid = validateForm();
+    
+        if (isValid) {
+            $(".form__error-message").hide();
+    
+            var formData = new FormData();
+            formData.append("photo", $(".form__loading-input-file input")[0].files[0]);
+            formData.append("expert", getSelectedExpert());
+            formData.append("description", $("#counter-text").val());
+    
+            $.ajax({
+                type: "POST",
+                url: "https://podruzhka.woman.ru/save_data/",
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function (response) {
+                    console.log("Данные успешно отправлены", response);
+                },
+                error: function (xhr, status, error) {
+                    console.error("Ошибка при отправке данных", xhr, status, error);
+                }
+            });
+        } else {
+            $(".form__error-message").show();
+            animateError();
+        }
+    });
+    
+    function validateForm() {
+        var photo = $(".form__loading-input-file input").val();
+        var expert = $(".form__radio-container-btn input:checked").val();
+        var description = $("#counter-text").val();
+    
+        return photo !== "" && expert !== undefined && description !== "";
+    }
+    
+    function animateError() {
+        for (let i = 0; i < 8; i++) {
+            setTimeout(function () {
+                $(".form__loading-input-file, .form__radio-container-btn-text").toggleClass("form__error");
+                $("#counter-text, .form__counter").toggleClass("form__error-text");
+            }, i * 350);
+        }
+    
+        setTimeout(function () {
+            $(".form__loading-input-file, .form__radio-container-btn-text").removeClass("form__error");
+            $("#counter-text, .form__counter").removeClass("form__error-text");
+        }, 5000);
+    }
+    
+    function getSelectedExpert() {
+        return $(".form__radio-container-btn input:checked").index() + 1;
+    }
 });
